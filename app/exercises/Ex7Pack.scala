30aae0e4cf9d087719e4e97f118138d948f70923
Jorge Galat <jrgglt@gmail.com>
2018-10-03 16:45:25 -0300

    Ex 7

--- app/exercises/Ex7Pack.scala
+++ app/exercises/Ex7Pack.scala
@@ -3,7 +3,18 @@ package exercises
 object Ex7Pack {
 
   def pack(list: List[Int]): List[List[Int]] = {
-    ???
+    list match {
+      case Nil => Nil
+      case x :: xs =>
+        pack(xs) match {
+          case Nil => List(List(x))
+          case y :: ys =>
+            if (y.contains(x))
+              (x::y) :: ys
+            else
+              List(x) :: y :: ys
+        }
+    }
   }
 
 }
