fb28e22d69ca111eb0abdfd93ad1831914a9e8a0
Jorge Galat <jrgglt@gmail.com>
2018-10-03 16:54:35 -0300

    Encode/Decode

--- app/exercises/Ex8Encode.scala
+++ app/exercises/Ex8Encode.scala
@@ -2,8 +2,10 @@ package exercises
 
 object Ex8Encode {
 
+  import exercises.Ex7Pack.pack
   def encode(list: List[Int]): List[(Int, Int)] = {
-    ???
+    val packList = pack(list)
+    pack.map(l => (l.length, l.head))
   }
 
 }
--- app/exercises/Ex9Decode.scala
+++ app/exercises/Ex9Decode.scala
@@ -5,7 +5,10 @@ object Ex9Decode {
   val customInput: List[(Int, Int)] = List((3, 1), (2, 2), (1, 3), (1, 4), (2, 5))
 
   def decode(list: List[(Int, Int)]): List[Int] = {
-    ???
+    for {
+      (r, e) <- list
+      _      <- 1 to r
+    } yield e
   }
 
 }
